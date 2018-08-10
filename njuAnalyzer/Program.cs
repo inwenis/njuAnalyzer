acfcc2e15fcf7e9a2eb0cb38f9e2ca332bf06a4d
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-08-10 10:51:24 +0200

    print expense details

--- njuAnalyzer/Program.cs
+++ njuAnalyzer/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace njuAnalyzer
 {
@@ -36,8 +38,35 @@ namespace njuAnalyzer
             Console.WriteLine(analyzer.GetCostDetails());
             Console.WriteLine();
 
+            ShowTotalCostForType(expenses, ExpenseType.InternationalCall);
+            ShowTotalCostForType(expenses, ExpenseType.InternationlSms);
+            ShowTotalCostForType(expenses, ExpenseType.SpecialSms);
+            ShowTotalCostForType(expenses, ExpenseType.InfolineCall);
+
+            Console.WriteLine();
+
+            var tataAnalyzer = new Analyzer(29, 10);
+            foreach (var expense in expenses2)
+            {
+                tataAnalyzer.Add(expense);
+            }
+
+            Console.WriteLine(tataAnalyzer.GetCostDetails());
+
+            ShowTotalCostForType(expenses2, ExpenseType.InternationalCall);
+            ShowTotalCostForType(expenses2, ExpenseType.InternationlSms);
+            ShowTotalCostForType(expenses2, ExpenseType.SpecialSms);
+            ShowTotalCostForType(expenses2, ExpenseType.InfolineCall);
+            ShowTotalCostForType(expenses2, ExpenseType.RoamingCall);
+
             Console.WriteLine("Press [enter] to exit");
             Console.ReadLine();
+        }
+
+        private static void ShowTotalCostForType(IEnumerable<Expense> expenses, ExpenseType type)
+        {
+            var sum = expenses.Where(x => x.ExpenseType == type).Sum(x => x.Charge);
+            Console.WriteLine(type + " " + sum);
         }
     }
 }
