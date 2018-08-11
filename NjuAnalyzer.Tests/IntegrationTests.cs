783375aeb4ba6a18b77b185e883e75ae9a07477f
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-08-11 07:23:22 +0200

    add integration test

---
+++ NjuAnalyzer.Tests/IntegrationTests.cs
@@ -0,0 +1,23 @@
+﻿using njuAnalyzer;
+using NUnit.Framework;
+
+[TestFixture]
+class IntegrationTests
+{
+    [Test]
+    public void CsvToExpenseList_And_Analyzer()
+    {
+        var lines = new string[]
+        {
+            "data i godzina;nr telefonu;liczba;zdarzenie;operator;koszt",
+            "2018.06.15 21:24;4542259472;2:15min:sek; Rozmowa głosowa;Dania kom;0.57 zł"
+        };
+        var result = CsvToExpenseList.Parse(lines);
+        var analyzer = new Analyzer(10, 29);
+        foreach (var expense in result)
+        {
+            analyzer.Add(expense);
+        }
+        Assert.AreEqual(0.57m, analyzer.GetTotalCost());
+    }
+}
